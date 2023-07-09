f40a97f3ce9c04208d4e0165758a2626d13eb578
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-07-09 11:47:38 -0300

    Commit 03

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 
 const app = express()
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(8800)
---
+++ api/routes/user.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const rotas = express.Router();
+
+export default rotas;
