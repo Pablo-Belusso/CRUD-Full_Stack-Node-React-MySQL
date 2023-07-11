30fa0a5c0de142068544b6239ae614fa99072c5f
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-07-11 11:28:17 -0300

    Commit 08

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -20,15 +20,47 @@ const InputArea = styled.div`
     
 `;
 
+
+const Input = styled.input`
+
+    width:  120px;
+    padding: 0 10px;
+    border: 1px solid #bbb;
+    border-radius: 5px;
+    height: 40px;
+`;
+
+const Label = styled.input``;
+
+
 const Form = ({ onEdit }) => {
     const ref = useRef();
 
     return (
         <FormContainer ref={ref}>
+
             <InputArea>
                 <label>Nome</label>
                 <Input name="nome" />
             </InputArea>
+
+            <InputArea>
+                <label>E-mail</label>
+                <Input name="email" type="email" />
+            </InputArea>
+
+            <InputArea>
+                <label>Telefone</label>
+                <Input name="fone" />
+            </InputArea>
+
+            <InputArea>
+                <label>Data de Nascimento</label>
+                <Input name="data_nascimento" type="date" />
+            </InputArea>
+
+            <Button type="submit">SALVAR</Button>
+
         </FormContainer>
     );
 };
