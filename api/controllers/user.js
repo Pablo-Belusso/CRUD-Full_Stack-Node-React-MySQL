05e26133b811b0042a448357b18ed87b81365026
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-07-12 16:39:01 -0300

    Commit 11

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,8 @@
 import { db } from "../db.js";
 
+
+// ---------- MÉTODO PARA CONSULTA DE DADOS (Visualizar os registros) ----------------------
+
 export const getUsers = (_, res) => {
 
     const q = "SELECT * FROM usuarios";
@@ -12,3 +15,44 @@ export const getUsers = (_, res) => {
 
     });
 };
+
+// ---------- MÉTODO PARA INSERIR DADOS ----------------------
+
+export const addUser = (req, res) => {
+    const q =
+      "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
+  
+    const values = [
+      req.body.nome,
+      req.body.email,
+      req.body.fone,
+      req.body.data_nascimento,
+    ];
+  
+    db.query(q, [values], (err) => {
+      if (err) return res.json(err);
+  
+      return res.status(200).json("Usuário criado com sucesso.");
+    });
+  };
+
+
+// ---------- MÉTODO PARA EDITAR / ATUALIZAR  DADOS ----------------------
+
+  export const updateUser = (req, res) => {
+    const q =
+      "UPDATE usuarios SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
+  
+    const values = [
+      req.body.nome,
+      req.body.email,
+      req.body.fone,
+      req.body.data_nascimento,
+    ];
+  
+    db.query(q, [...values, req.params.id], (err) => {
+      if (err) return res.json(err);
+  
+      return res.status(200).json("Usuário atualizado com sucesso.");
+    });
+  };
